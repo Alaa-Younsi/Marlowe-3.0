a172ebdcb2ca48a3df6dc7882c12a5b0e146d813
Alaa Younsi <alaayounsi777@gmail.com>
2023-05-09 00:29:48 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,591 @@
+
+$(document).ready(function(){
+
+    $("#Home").click(function(){
+        $("#Home-text").fadeIn(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Info").click(function(){
+        $("#Home-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Certificates").click(function(){
+        $("#Home-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Projects").click(function(){
+        $("#Home-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Blog").click(function(){
+        $("#Home-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Contact").click(function(){
+        $("#Home-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Off").click(function(){
+        $("#Home-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Home").click(function(){
+        $("#Main-image").fadeIn(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Info").click(function(){
+        $("#Info-text").fadeIn(1);
+      });
+  }); 
+
+$(document).ready(function(){
+
+    $("#Home").click(function(){
+        $("#Info-text").fadeOut(1);
+      });
+  }); 
+
+$(document).ready(function(){
+
+    $("#Certificates").click(function(){
+        $("#Info-text").fadeOut(1);
+      });
+  }); 
+
+$(document).ready(function(){
+
+    $("#Projects").click(function(){
+        $("#Info-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Blog").click(function(){
+        $("#Info-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Contact").click(function(){
+        $("#Info-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Certificates").click(function(){
+        $("#Certificates-text").fadeIn(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Info").click(function(){
+        $("#Certificates-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Home").click(function(){
+        $("#Certificates-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Projects").click(function(){
+        $("#Certificates-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Blog").click(function(){
+        $("#Certificates-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Contact").click(function(){
+        $("#Certificates-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Off").click(function(){
+        $("#Certificates-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Projects").click(function(){
+        $("#Projects-text").fadeIn(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Home").click(function(){
+        $("#Projects-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Info").click(function(){
+        $("#Projects-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Certificates").click(function(){
+        $("#Projects-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Blog").click(function(){
+        $("#Projects-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Contact").click(function(){
+        $("#Projects-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Off").click(function(){
+        $("#Projects-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Blog").click(function(){
+        $("#Blog-text").fadeIn(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Home").click(function(){
+        $("#Blog-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Info").click(function(){
+        $("#Blog-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Certificates").click(function(){
+        $("#Blog-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Projects").click(function(){
+        $("#Blog-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Contact").click(function(){
+        $("#Blog-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Off").click(function(){
+        $("#Blog-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Contact").click(function(){
+        $("#Contact-text").fadeIn(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Home").click(function(){
+        $("#Contact-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Info").click(function(){
+        $("#Contact-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Certificates").click(function(){
+        $("#Contact-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Projects").click(function(){
+        $("#Contact-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Blog").click(function(){
+        $("#Contact-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Off").click(function(){
+        $("#Contact-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Info").click(function(){
+        $("#Info-text-tv").fadeIn(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Home").click(function(){
+        $("#Info-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Certificates").click(function(){
+        $("#Info-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Projects").click(function(){
+        $("#Info-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Blog").click(function(){
+        $("#Info-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Contact").click(function(){
+        $("#Info-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Off").click(function(){
+        $("#Info-text-tv").fadeOut(1);
+      });
+  });
+$(document).ready(function(){
+
+    $("#Certificates").click(function(){
+        $("#Certificates-text-tv").fadeIn(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Home").click(function(){
+        $("#Certificates-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Info").click(function(){
+        $("#Certificates-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Projects").click(function(){
+        $("#Certificates-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Blog").click(function(){
+        $("#Certificates-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Contact").click(function(){
+        $("#Certificates-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Off").click(function(){
+        $("#Certificates-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Projects").click(function(){
+        $("#Projects-text-tv").fadeIn(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Home").click(function(){
+        $("#Projects-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Info").click(function(){
+        $("#Projects-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Certificates").click(function(){
+        $("#Projects-text-tv").fadeOut(1);
+      });
+  });
+
+ $(document).ready(function(){
+
+    $("#Blog").click(function(){
+        $("#Projects-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Contact").click(function(){
+        $("#Projects-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Off").click(function(){
+        $("#Projects-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Blog").click(function(){
+        $("#Blog-text-tv").fadeIn(1);
+      });
+  });
+  
+$(document).ready(function(){
+
+    $("#Home").click(function(){
+        $("#Blog-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Info").click(function(){
+        $("#Blog-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Certificates").click(function(){
+        $("#Blog-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Projects").click(function(){
+        $("#Blog-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Contact").click(function(){
+        $("#Blog-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Off").click(function(){
+        $("#Blog-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Contact").click(function(){
+        $("#Contact-text-tv").fadeIn(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Home").click(function(){
+        $("#Contact-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Info").click(function(){
+        $("#Contact-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Certificates").click(function(){
+        $("#Contact-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Projects").click(function(){
+        $("#Contact-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Blog").click(function(){
+        $("#Contact-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Off").click(function(){
+        $("#Contact-text-tv").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Off").click(function(){
+        $("#Tv").slideUp(2000);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Off").click(function(){
+        $("#Main-image").slideUp(2000);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Off").click(function(){
+        $("#Projects-text").slideUp(2000);
+      });
+  });
+
+  $(document).ready(function(){
+
+    $("#Off").click(function(){
+        $("#Info-text").fadeOut(1);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#Off").click(function(){
+        $("#On-button").fadeIn(7000);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#On-button").click(function(){
+        $("#Tv").slideDown(2000);
+      });
+  });
+
+$(document).ready(function(){
+
+    $("#On-button").click(function(){
+        $("#On-button").fadeOut(1);
+      });
+  });
+
+
+
+
---
+++ main2.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#Front-page").fadeIn(0.1);
+  });
+
+$(document).ready(function(){
+    $("#Front-page").fadeOut(3500);
+  });
+
+$(document).ready(function(){
+    $("#Marlowe-portfolio").fadeIn(250);
+  });
+
+$(document).ready(function(){
+    $("#Marlowe-portfolio").fadeOut(750);
+  });
